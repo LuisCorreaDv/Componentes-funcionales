1288880aafdb8c87b47ebc798f02f0b604f36d94
LuisCorreaDv <dev.luis.correa@gmail.com>
2023-09-25 17:43:52 -0600

    Mejorando el layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
 import FormSignUp from './components/FormSignUp';
+import Container from '@mui/material/Container';
 
 function App() {
   return (
     <>
-      <h1>Formulario Registro</h1>
-      <FormSignUp />
+      <Container component="section" maxWidth="sm">
+        <h1>Formulario Registro</h1>
+        <FormSignUp />
+      </Container>
     </>
   );
 }
