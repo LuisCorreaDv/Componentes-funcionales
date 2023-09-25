5aa3e4bee97d63258b354df3ae448a3b825367cc
LuisCorreaDv <dev.luis.correa@gmail.com>
2023-09-25 16:06:44 -0600

    Importando Material UI y utlizando sus componentes

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,7 +1,18 @@
+import Button from '@mui/material/Button';
+
 function FormSignUp() {
     return <form>
+        <label>Nombre</label>
         <input type="text" />
-        <button>Registrarse</button>
+        <label>Apellidos</label>
+        <input type="text" />
+        <label>Correo Electrónico</label>
+        <input type="email" />
+        <label>Promociones</label>
+        <input type="checkbox" />
+        <label>Novedades</label>
+        <input type="checkbox" />
+        <Button variant="contained" color="success">Registrarse</Button>
     </form>
 }
 
