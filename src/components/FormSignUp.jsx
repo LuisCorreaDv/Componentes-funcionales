4d693355a4f4358277216561f651281a12c16f7a
LuisCorreaDv <dev.luis.correa@gmail.com>
2023-09-25 20:06:37 -0600

    Manejador de informacion para el formulario

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -5,7 +5,7 @@ import Switch from '@mui/material/Switch';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 
-function FormSignUp() {
+function FormSignUp({handleSubmit}) {
     const [name, setName] = useState("")
     const [lastName, setLastName] = useState("")
     const [email, setEmail] = useState("")
@@ -14,7 +14,13 @@ function FormSignUp() {
 
     return <form onSubmit={(e) => {
         e.preventDefault()
-        console.log({name, lastName, email, prom, nov})
+        handleSubmit({
+            name, 
+            lastName, 
+            email, 
+            prom, 
+            nov
+        })
     }}>
         <TextField
             id="name"
